3eb244e62f6946aaf908a1a55375b7256a4d6de5
RubensKj <rubenskleinschmidtjr@gmail.com>
2019-09-02 08:55:31 -0300

    Editing service method

--- src/main/java/br/com/ipet/Controllers/Service/ServiceCrudController.java
+++ src/main/java/br/com/ipet/Controllers/Service/ServiceCrudController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -62,6 +63,46 @@ public class ServiceCrudController {
             }
         } else {
             return ResponseEntity.ok("Product was empty");
+        }
+    }
+
+    @GetMapping("/services-list/{id}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('OWNER')")
+    public ResponseEntity<?> findProductById(@PathVariable long id, HttpServletRequest req) {
+        String jwtToken = jwtProvider.getJwt(req);
+        String emailOwnerLogged = jwtProvider.getEmailFromJwtToken(jwtToken);
+
+        if (emailOwnerLogged != null) {
+            if (serviceService.existsById(id)) {
+                return ResponseEntity.ok(serviceService.findById(id));
+            } else {
+                return new ResponseEntity<>("Doesn't exists any service with this id!",
+                        HttpStatus.NOT_FOUND);
+            }
+        } else {
+            return new ResponseEntity<>("Any company connected on application",
+                    HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @PostMapping("/edit-service/{id}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('OWNER')")
+    public ResponseEntity<?> editProduct(@PathVariable long id, @Valid @RequestBody Service service, HttpServletRequest req) {
+        if (service != null && serviceService.existsById(id)) {
+            String jwtToken = jwtProvider.getJwt(req);
+            String emailOwnerLogged = jwtProvider.getEmailFromJwtToken(jwtToken);
+
+            if (!emailOwnerLogged.isEmpty()) {
+                serviceService.save(service);
+                return new ResponseEntity<>("Service was edited",
+                        HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Any company connected on application",
+                        HttpStatus.FORBIDDEN);
+            }
+        } else {
+            return new ResponseEntity<>("Product is empty",
+                    HttpStatus.NOT_FOUND);
         }
     }
 
