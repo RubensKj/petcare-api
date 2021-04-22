8e54bfeec3dd8cfb99b8910409cd5e33874502cb
Rubens Kleinschmidt Júnior <36218162+RubensKj@users.noreply.github.com>
2021-04-21 23:42:21 -0300

    Update EvaluationCrudController.java

--- src/main/java/br/com/ipet/Controllers/Evaluation/EvaluationCrudController.java
+++ src/main/java/br/com/ipet/Controllers/Evaluation/EvaluationCrudController.java
@@ -29,9 +29,9 @@ import java.util.Set;
         "https://aw-petcare-business.herokuapp.com",
         "http://aw-petcare-client.herokuapp.com",
         "http://aw-petcare-business.herokuapp.com",
-        "https://petcare-client.now.sh/",
-        "https://petcare-client.now.sh",
-        "https://petcare-business.now.sh"
+        "https://petcare-client.vercel.app/",
+        "https://petcare-client.vercel.app",
+        "https://petcare-business.vercel.app"
 })
 @RestController
 @RequestMapping("/api")
