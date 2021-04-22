0913f7a439dbbcbc2d1a7c2eca986d211456cfd3
Rubens Kleinschmidt Júnior <36218162+RubensKj@users.noreply.github.com>
2021-04-21 23:42:07 -0300

    Update AddressActionsController.java

--- src/main/java/br/com/ipet/Controllers/Address/AddressActionsController.java
+++ src/main/java/br/com/ipet/Controllers/Address/AddressActionsController.java
@@ -15,9 +15,9 @@ import javax.validation.Valid;
         "https://aw-petcare-business.herokuapp.com",
         "http://aw-petcare-client.herokuapp.com",
         "http://aw-petcare-business.herokuapp.com",
-        "https://petcare-client.now.sh/",
-        "https://petcare-client.now.sh",
-        "https://petcare-business.now.sh"
+        "https://petcare-client.vercel.app/",
+        "https://petcare-client.vercel.app",
+        "https://petcare-business.vercel.app"
 })
 @RestController
 @RequestMapping("/api")
