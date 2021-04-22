7d16278809c679ba616d946132a64e0a9bf7e791
Rubens Kleinschmidt Júnior <36218162+RubensKj@users.noreply.github.com>
2021-04-21 23:44:01 -0300

    Update UserActionsController.java

--- src/main/java/br/com/ipet/Controllers/User/UserActionsController.java
+++ src/main/java/br/com/ipet/Controllers/User/UserActionsController.java
@@ -26,9 +26,9 @@ import javax.servlet.http.HttpServletRequest;
         "https://aw-petcare-business.herokuapp.com",
         "http://aw-petcare-client.herokuapp.com",
         "http://aw-petcare-business.herokuapp.com",
-        "https://petcare-client.now.sh/",
-        "https://petcare-client.now.sh",
-        "https://petcare-business.now.sh"
+        "https://petcare-client.vercel.app/",
+        "https://petcare-client.vercel.app",
+        "https://petcare-business.vercel.app"
 })
 @RestController
 @RequestMapping("/api/users")
